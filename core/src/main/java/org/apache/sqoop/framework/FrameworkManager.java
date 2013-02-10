ba72aeda4d71ff9d759aa6fe2a0f931e8aaa83d3
Cheolsoo Park <cheolsoo@apache.org>
2013-02-10 13:27:18 -0800

    SQOOP-880: NPE when stopping job that was never executed
    (Jarcec Cecho via Cheolsoo Park)

--- core/src/main/java/org/apache/sqoop/framework/FrameworkManager.java
+++ core/src/main/java/org/apache/sqoop/framework/FrameworkManager.java
@@ -525,7 +525,7 @@ public final class FrameworkManager {
     Repository repository = RepositoryManager.getInstance().getRepository();
     MSubmission submission = repository.findSubmissionLastForJob(jobId);
 
-    if(!submission.getStatus().isRunning()) {
+    if(submission == null || !submission.getStatus().isRunning()) {
       throw new SqoopException(FrameworkError.FRAMEWORK_0003,
         "Job with id " + jobId + " is not running");
     }
